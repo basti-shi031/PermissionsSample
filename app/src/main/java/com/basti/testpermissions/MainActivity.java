2f608f04897c4beca7dcfce5940f33cfdff2c4be
basti-shi031 <876572078@qq.com>
2016-01-18 15:29:12 +0800

    Initial commit

---
+++ app/src/main/java/com/basti/testpermissions/MainActivity.java
@@ -0,0 +1,63 @@
+package com.basti.testpermissions;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btWrite;
+    private int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initEvent();
+    }
+
+    private void initEvent() {
+        btWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    //申请WRITE_EXTERNAL_STORAGE权限
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+                } else {
+                    btWrite.setText("授权成功123");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+                Toast.makeText(getApplicationContext(),"授权成功",Toast.LENGTH_SHORT).show();
+                btWrite.setText("授权成功");
+            } else {
+                // Permission Denied
+                Toast.makeText(getApplicationContext(),"授权拒绝",Toast.LENGTH_SHORT).show();
+                btWrite.setText("授权拒绝");
+            }
+        }
+    }
+
+    private void initView() {
+
+        btWrite = (Button) findViewById(R.id.write);
+    }
+}
